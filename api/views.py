254925c89a669eacc5b99cecdb662beb412d3824
hereliesadvaith <hereliesadvaith@gmail.com>
2023-04-28 23:02:39 +0530

    views.py

--- api/views.py
+++ api/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
+
+
+def getRoutes(request):
+    # safe false to return more than just a python dictionary
+    return JsonResponse("Our Api", safe=False)
