fb64d394f182ce30f3b0b70b90e4a59d31faa7df
hereliesadvaith <hereliesadvaith@gmail.com>
2023-04-29 06:24:19 +0530

    models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+# this is where we create our database
+
+
+class Note(models.Model):
+    # attributes works as columns and instances work as rows
+    body = models.TextField(null=True, blank=True)
+    update = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        # returns the first 50 characters
+        return self.body[0:50]
