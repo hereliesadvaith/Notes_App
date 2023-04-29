ba45c828926bd9764d1cea7540040af1040e3ec9
hereliesadvaith <hereliesadvaith@gmail.com>
2023-04-29 11:30:39 +0530

    frontend

--- api/models.py
+++ api/models.py
@@ -6,6 +6,7 @@ from django.db import models
 
 class Note(models.Model):
     # attributes works as columns and instances work as rows
+    title = models.TextField(null=True, blank=True)
     body = models.TextField(null=True, blank=True)
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
--- mynotes/settings.py
+++ mynotes/settings.py
@@ -39,10 +39,12 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "api.apps.ApiConfig",
     "rest_framework",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -123,3 +125,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+]
